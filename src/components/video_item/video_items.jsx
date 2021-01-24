43ab96041be7d841dbfc9c5a2ca52bd41fe58fe9
zlzlzlmo <zlzlzlmo@daum.net>
2021-01-24 14:38:27 +0900

    동영상 리스트 레이아웃

--- src/components/video_item/video_items.jsx
+++ src/components/video_item/video_items.jsx
@@ -1,6 +1,19 @@
-const VideoItem = (props)=>{
+import styles from "./video_item.module.css";
+const VideoItem = ({ video }) => {
   return (
-    <h1>{props.video.title}</h1>
-  )
-}
-export default VideoItem
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={video.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{video.title}</p>
+          <p className={styles.channel}>{video.channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
+};
+export default VideoItem;
